d16813fa2dab4ef44913648098ffade8b5ebd39a
LimJinsung <llllil1@inha.edu>
2021-09-14 04:14:30 +0900

    version4

--- src/ReviewDemo01.java
+++ src/ReviewDemo01.java
@@ -8,11 +8,22 @@ public class ReviewDemo01 {
         //int cnt = 0;
         boolean isPrime = true;
 
-        for(int k=2; k<number; k++){ // 반복 횟수 2회 감소
-            if(number % k == 0)// 입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
+//        for(int k=2; k<number; k++){ // 반복 횟수 2회 감소
+//            if(number % k == 0) {// 입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
+//                isPrime = false;
+//                break; // 첫번째 약수가 발견되면 for문 탈출
+//            }
+//            //ystem.out.println(k);
+//        }
+        int k=2;
+        while(k<number){ // 반복 횟수 2회 감소
+            if(number % k == 0) {// 입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
                 isPrime = false;
+                break; // 첫번째 약수가 발견되면 for문 탈출
+            }
+            //ystem.out.println(k);
+            k++;
         }
-
         if(isPrime) // isPrime 변수 값이 true 면
             System.out.println(number + "은(는) 소수!");
         else
